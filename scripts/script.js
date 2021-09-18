12a7972456b7e3d4ee152e59ded1333e4bf300e4
linhdoan9513 <78831085+linhdoan9513@users.noreply.github.com>
2021-09-18 12:48:35 -0400

    Updated script file with global data after fetching

--- scripts/script.js
+++ scripts/script.js
@@ -5,19 +5,34 @@ const myApp = {};
 myApp.globalTimeline = "https://corona-api.com/timeline";
 
 // Use Fetch API to get all relevant global data
-myApp.getData = () => {
-    const url = new URL(myApp.globalTimeline)
-    fetch(url)
-        .then(response => response.json())
-        .then(jsonResponse => jsonResponse.data[0]);
-}
+myApp.getGlobalData = () => {
+  const url = new URL(myApp.globalTimeline);
+  fetch(url)
+    .then((response) => response.json())
+    .then((jsonResponse) => myApp.displayGlobalData(jsonResponse.data[0]));
+};
+
+//Display global API to the HTML page
+//Create li element to display active properties, confirmed case, date, deaths, updated_at
+//Create li for each one of this property
+//Create an local array of specific relevant properties and use them to access the properties value of the data object
+
+myApp.displayGlobalData = (data) => {
+  const globalList = document.querySelector("#globalList");
+  const globalInfo = ["active", "confirmed", "date", "deaths", "updated_at"];
+  for (let i = 0; i < globalInfo.length; i++) {
+    //create list elements
+    const listElements = document.createElement("li");
+    listElements.append(`${globalInfo[i]} : ${data[globalInfo[i]]}`);
+    globalList.append(listElements);
+  }
+};
 
 // Declare an initialization method
 myApp.init = () => {
-
-    // Call the getData method 
-    myApp.getData();
-}
+  // Call the getData method
+  myApp.getGlobalData();
+};
 
 // Call the initialization method
 myApp.init();
